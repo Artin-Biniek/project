9f6c8612c6abd9db0772979008e911dbdcfea650
Artin-Biniek <71418717+Artin-Biniek@users.noreply.github.com>
2024-05-27 06:04:04 -0400

    A file regarding step_one
    In this commit I added for an example attribute how to go about checking its validity for the parent_attribute, and child_attribute which the same idea can be applied to other existing attributes.

---
+++ step_one.py
@@ -0,0 +1,68 @@
+import json
+"""
+1)Identify all attributes,all types,all domains
+
+
+"""
+
+#Example
+json_string= '''
+{
+    "@context":{
+        "@id":"https://www.w3.org/ns/credentials/v2",
+        "@type": "https://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+    }
+}
+
+'''
+
+dict={
+    "@context":["@id","@type"]
+    
+    
+    
+    }
+parent_attributes=["@context"]
+X="tempString"
+
+#1)Automatically identify the parent_attributes for a given JSON
+data=json.loads(json_string)
+for attribute in data:
+    #Check to see if attribute is a valid attribute
+    if(attribute in parent_attributes):
+        X=attribute
+        print(attribute)
+    else:
+        print("Invalid Json")
+        exit()
+
+
+#2)Automatically identify the child attributes
+
+#Obtain the child attributes
+child_attributes=data.get(X,{})
+
+#Obtain the valid attributes for the given attribute
+valid_attributes=dict[X]
+
+
+#Iterate over the valid child attributes    
+for key in child_attributes:
+    for child in valid_attributes:
+        if(child==key):
+            #If valid we dont need to check this attribute anymore
+            valid_attributes.pop(0)
+            print(key)
+            print(child)
+            break
+        else:
+            print(key)
+            print(child)
+            print("Invalid JSON")
+            exit()
+
+
+#Next we check all domains
+
+#To check for valid values see if it matches the conditions for a value
+
