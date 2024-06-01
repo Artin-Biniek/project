fcc48b91a193f18a37043c5f697f196d9426ea0e
Artin-Biniek <71418717+Artin-Biniek@users.noreply.github.com>
2024-05-31 20:34:28 -0400

    Add files via upload

--- step_one.py
+++ step_one.py
@@ -1,116 +1,91 @@
 import json
+import validators
+import re
 """
 1)Identify all attributes,all types,all domains
-
+2)Future checks parameter and value relationship check
+3)Parent and child checks
+4)parent and child of child.... relationship checks.
 
 """
 """
 
 1)Pass in JSON
 2)Iterate over elements of JSON
-3)Check if elements belong to one of the valid attributes and check if its a dictionary
-4)If dictionary pass it once again as the new JSON
+3)Check if elements belong to oe of the valid attributes and check if its a dictionary
+4)If dictionary pass it oncen again as the new JSON
 5)Keep going until you find that its no longer a dictionary value
 6)It will automatically stop at that value and continue on with the next elem in the list when
 those child elements are done it will move to the next parent elements and so on.
 """
-#Checking valid attributes
-def test(X):
+
+#Building dictionary
+def buildingDict(X):
+   dictTemp={}
+   for elem in X:
+    if isinstance(X[elem],dict):
+        print("test")
+        dictTemp |= {elem:X[elem]}
+        #print(dictTemp)
+    elif isinstance(X[elem],list):
+        dictTemp |= {elem:X[elem]}
+    return dictTemp
+
+#Checking to see if param:Value exist in dict like is there a way to go to that specified param and value?
+def temp(dictTemp,X):
     for elem in X:
-        if elem in attributes and isinstance(X[elem],dict):
-            print(elem)
-            #Obtaining child attributes
-            print(X[elem])
-            test(X[elem])
-        elif elem in attributes and not isinstance(X[elem],dict):
-            #print(elem)
-            #print(X[elem])
-            test2(X[elem])
-        else:
-            print("invalid")
-            exit()
+        #print(elem)
+        for elem2 in dictTemp:
+            #print(elem2)
+            if(elem==elem2):
+                print(elem, "and",elem2,"are the same attribute")
+                if isinstance(X[elem],dict) and isinstance(dictTemp[elem2],dict) :
+                    temp(dictTemp[elem2],X[elem])
+
+                #checking if value type is same
+                elif not isinstance(X[elem],dict) and not isinstance(dictTemp[elem2],dict) :
+                    if type(X[elem])==type(dictTemp[elem2]):
+                        if isinstance(X[elem],str) and isinstance(dictTemp[elem2],str):
+                                if validators.url(X[elem]) and validators.url(dictTemp[elem2]):
+                                    print("It is a url type")
+                    else:
+                        print(type(X[elem]),"and",type(dictTemp[elem2]),"have the same value type")
 
 
-"""
-context:
-is a valid url that first elem is https://www.w3.org/ns/credentials/v2
-the next 1...n elements is any existing url https:// || http:// (a string type)
-
-Id(as a parent):
-an existing url http://(a string type)
-
-Id(as a child of credential subject)
-did:sometext:sometext(a string type)
-
-Type(parent,can be a child of Vc object,verifiable presentation object,credentialStatus object,termsOfUse object,Evidence object):
-[someName,....,SomeNameN](string type) or Absolute Url [https://www.something.com...](string)
-
-Names:
-Can be a string "SomeName" or language value object
-
-language value object
-contains @value(a string value),@language(language tag),and may contain @direction(base direction string).
-
-
-
-
-"""
-#Type checking
-def test2(elem):
-    print("temp")
-    if elem in values:
-        print(type(elem))
-    else:
-        print("invalid")
-        exit()
 
 
 #Example
+
 json_string= '''
 {
     "@context":{
-        "@type":{
-            "aud":"@id",
-            "@protected": 100
+        "@protected":true,
+        "@vocab": "https://www.w3.org/ns/credentials/issuer-dependent#",
+        "kid":{
+            "@type": "w3c.org"
 
         }
     }
 }
-
 '''
-def get_keys(data):
-    keys = []
-    if isinstance(data, dict):
-        for key, value in data.items():
-            keys.append(key)
-            keys += get_keys(value)
-    elif isinstance(data, list):
-        for value in data:
-            keys += get_keys(value)
-    return keys
-
-def get_values(data):
-    values = []
-    if isinstance(data, dict):
-        for value in data.values():
-            values += get_values(value)
-    elif isinstance(data, list):
-        for item in data:
-            values += get_values(item)
-    else:
-        values.append(data)
-    return values
 
 
 with open('op.txt', 'r') as file:
     data = json.load(file)
 
+dictTemp={}
+dictTemp=buildingDict(data)
+#print(dictTemp["@context"])
+#dictTemp.update({key1,val1})
 
-attributes=set(get_keys(data))
+"""
+ValueType:{"@type":"url"}
+IdType:{"@Id":"did:something:something2",url}
 
-values=set(get_values(data))
+"""
 
-#attributes=["@context","@id","@id2"]
+
 data=json.loads(json_string)
-test(data)
+temp(dictTemp,data)
+
 
