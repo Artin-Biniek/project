6cb16c7f7bab21fcc3fbfbc77c70f3038d60dd64
Artin-Biniek <71418717+Artin-Biniek@users.noreply.github.com>
2024-06-03 15:36:48 -0400

    Add files via upload

--- step_one.py
+++ step_one.py
@@ -1,81 +1,70 @@
 import json
-import validators
-import re
 """
 1)Identify all attributes,all types,all domains
-2)Future checks parameter and value relationship check
-3)Parent and child checks
-4)parent and child of child.... relationship checks.
+
 
 """
 
-#Building dictionary
-def buildingDict(X):
-   dictTemp={}
-   for elem in X:
-    if isinstance(X[elem],dict):
-        print("test")
-        dictTemp |= {elem:X[elem]}
-        #print(dictTemp)
-    elif isinstance(X[elem],list):
-        dictTemp |= {elem:X[elem]}
-    return dictTemp
-
-#Checking to see if param:Value exist in dict like is there a way to go to that specified param and value?
-def temp(dictTemp,X):
-    for elem in X:
-        #print(elem)
-        for elem2 in dictTemp:
-            #print(elem2)
-            if(elem==elem2):
-                print(elem, "and",elem2,"are the same attribute")
-                if isinstance(X[elem],dict) and isinstance(dictTemp[elem2],dict) :
-                    temp(dictTemp[elem2],X[elem])
-
-                #checking if value type is same
-                elif not isinstance(X[elem],dict) and not isinstance(dictTemp[elem2],dict) :
-                    if type(X[elem])==type(dictTemp[elem2]):
-                        if isinstance(X[elem],str) and isinstance(dictTemp[elem2],str):
-                                if validators.url(X[elem]) and validators.url(dictTemp[elem2]):
-                                    print("It is a url type")
-                    else:
-                        print(type(X[elem]),"and",type(dictTemp[elem2]),"have the same value type")
-
-
-
-
 #Example
-
 json_string= '''
 {
     "@context":{
-        "@protected":true,
-        "@vocab": "https://www.w3.org/ns/credentials/issuer-dependent#",
-        "kid":{
-            "@type": "w3c.org"
-
-        }
+        "@id":"https://www.w3.org/ns/credentials/v2",
+        "@type": "https://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+    
     }
 }
+
 '''
 
-
-with open('op.txt', 'r') as file:
-    data = json.load(file)
-
-dictTemp={}
-dictTemp=buildingDict(data)
-#print(dictTemp["@context"])
-#dictTemp.update({key1,val1})
-
-"""
-ValueType:{"@type":"url"}
-IdType:{"@Id":"did:something:something2",url}
-
-"""
+parent_attributes=["@context"]
+#Concerned about child attributes
+dict2={
+    "@context":["@id","@type"]
+    
+    
+    
+    }
+#Concerned about values of child attributes or of parent attributes
+dict3={
 
 
+}
+
+
+
+X="tempString"
+
+#1)Automatically identify the parent_attributes for a given JSON
 data=json.loads(json_string)
-temp(dictTemp,data)
+for attribute in data:
+    #Check to see if attribute is a valid attribute
+    if(attribute in parent_attributes):
+        X=attribute
+        print(attribute)
+    else:
+        print("Invalid Json")
+        exit()
 
+
+#2)Automatically identify the child attributes
+
+
+#Obtain the child attributes
+child_attributes=data.get(X)
+print(child_attributes)
+
+print(dict2[X])
+if isinstance(child_attributes, dict):
+    for key in child_attributes:
+        if(key in dict2[X]):
+            print("True")
+        else:
+            print("Invalid Json")
+            exit()
+
+print("It passed")
+#Next we check all domains
+
+#To check for valid values see if it matches the conditions for a value
 
